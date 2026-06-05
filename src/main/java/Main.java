a95374586abaf50a43e2bb216efbcc2ac975b53d
clovers1111 <hjcpaget@gmail.com>
2026-06-05 18:51:43 -0400

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
 import com.google.gson.Gson;
+
+import java.util.Optional;
 // import com.dampcake.bencode.Bencode; - available if you need it!
 
 public class Main {
@@ -9,24 +11,23 @@ public class Main {
     System.err.println("Logs from your program will appear here!");
     
     String command = args[0];
+    String decodedOutput = "";
     if("decode".equals(command)) {
       String bencodedValue = args[1];
-      String decoded;
-      try {
-        decoded = decodeBencode(bencodedValue);
-      } catch(RuntimeException e) {
-        System.out.println(e.getMessage());
-          return;
+      if (Character.isDigit(bencodedValue.charAt(0))) {
+        final String decodedString = decodeBencodeString(bencodedValue);
+        decodedOutput = gson.toJson(decodedString);
+      } else if (Character.isAlphabetic(bencodedValue.charAt(0))) {
+        decodedOutput = decodeBencodeInteger(bencodedValue);
       }
-      System.out.println(decoded);
-
     } else {
       System.out.println("Unknown command: " + command);
     }
+    System.out.println(decodedOutput);
 
   }
 
-  static String decodeBencode(String bencodedString) {
+  static String decodeBencodeString(String bencodedString) {
     if (Character.isDigit(bencodedString.charAt(0))) {
       int firstColonIndex = 0;
       for(int i = 0; i < bencodedString.length(); i++) { 
@@ -37,12 +38,13 @@ public class Main {
       }
       int length = Integer.parseInt(bencodedString.substring(0, firstColonIndex));
       return bencodedString.substring(firstColonIndex+1, firstColonIndex+1+length);
-    } else if (Character.isAlphabetic(bencodedString.charAt(0))) {
-      return bencodedString.substring(1, bencodedString.length()-1);
-    }
-    else {
+    } else {
       throw new RuntimeException("Only strings and integers are supported at the moment");
     }
+  }
+
+  static String decodeBencodeInteger(String bencodedInteger) {
+    return bencodedInteger.substring(1, bencodedInteger.length()-1);
   }
   
 }
