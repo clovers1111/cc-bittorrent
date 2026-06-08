c1760fe145ee9f7f58aafb342d475d1b285a30ab
clovers1111 <hjcpaget@gmail.com>
2026-06-07 20:30:54 -0400

    codecrafters submit [skip ci]

--- src/main/java/DecodeMetadata.java
+++ src/main/java/DecodeMetadata.java
@@ -6,8 +6,7 @@ public record DecodeMetadata(Object value, int nextIndex) {
 
     public DecodeInfo toDecodeInfo() {
         final Gson gson = new Gson();
-        final String value = new String((byte[]) value(), StandardCharsets.ISO_8859_1);
-        return gson.fromJson(value, DecodeInfo.class);
+        return gson.fromJson(value().toString(), DecodeInfo.class);
     }
 }
 
